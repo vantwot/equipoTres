13e67cb3bb6607b21f641b705144c40da537fe94
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-05-10 21:21:47 -0500

    Trying to fix navigation to home fragment

--- app/src/main/java/com/example/project1/view/HomeFragment.kt
+++ app/src/main/java/com/example/project1/view/HomeFragment.kt
@@ -8,20 +8,27 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.project1.R
 import android.widget.Button
+import com.example.project1.databinding.FragmentHomeBinding
+import com.example.project1.databinding.FragmentLoginBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button1.setOnClickListener {
+        binding.button1.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_createFragment)
         }
     }
