1ed448c1c112c75734783cac8f531a7be7634e38
Van <45314803+vantwot@users.noreply.github.com>
2024-05-14 15:28:05 -0500

    update navegación

--- app/src/main/java/com/example/project1/view/HomeFragment.kt
+++ app/src/main/java/com/example/project1/view/HomeFragment.kt
@@ -5,22 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.project1.R
 import com.example.project1.databinding.FragmentEditAppointmentBinding
 import com.example.project1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    //private  var _binding: FragmentHomeBinding? = null
-
-    //private  val binding get() = _binding!!
+    private  var _binding: FragmentHomeBinding? = null
+    private  val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        nav_createView()
+        return  binding.root
 
+    }
+
+    private fun nav_createView(){
+        binding.button1.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_createFragment)
+        }
     }
 
 }
