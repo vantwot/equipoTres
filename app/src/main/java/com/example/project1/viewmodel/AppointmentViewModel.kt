884f7ffdf9187f8fc5ba9208bdc9a9a5e619f277
Nicol_O <nicol.ortiz@correounivalle.edu.co>
2024-05-14 06:13:34 -0500

    comments deleted

--- app/src/main/java/com/example/project1/viewmodel/AppointmentViewModel.kt
+++ app/src/main/java/com/example/project1/viewmodel/AppointmentViewModel.kt
@@ -10,30 +10,5 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class AppointmentViewModel : ViewModel() {
-/**
-    // Función para obtener la lista de razas de perros
-    fun getDogBreeds(onSuccess: (List<String>) -> Unit, onError: (String) -> Unit) {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://dog.ceo/api/breeds/list/all")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
-        val service = retrofit.create(DogService::class.java)
-
-        service.getBreeds().enqueue(object : Callback<BreedResponse> {
-            override fun onResponse(call: Call<BreedResponse>, response: Response<BreedResponse>) {
-                if (response.isSuccessful) {
-                    val breeds = response.body()?.breeds ?: emptyList()
-                    onSuccess(breeds)
-                } else {
-                    onError("Error en la respuesta de la API")
-                }
-            }
-
-            override fun onFailure(call: Call<BreedResponse>, t: Throwable) {
-                Log.e("AppointmentViewModel", "Error al llamar a la API: ${t.message}")
-                onError("Error al llamar a la API")
-            }
-        })
-    } */
 }
