c2e5a3f64daa77c39d02087a9321c7595c7cc636
Van <45314803+vantwot@users.noreply.github.com>
2024-05-14 15:15:45 -0500

    update

--- app/src/main/java/com/example/project1/view/HomeFragment.kt
+++ app/src/main/java/com/example/project1/view/HomeFragment.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.project1.R
+import com.example.project1.databinding.FragmentEditAppointmentBinding
+import com.example.project1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    //private  var _binding: FragmentHomeBinding? = null
+
+    //private  val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
+
 }
+
